34cc8d315f857e7e398205c18c3a20cfe39f3008
Tanner Smith <tanner@tssoftware.net>
2013-08-04 17:02:58 -0700

    Begin API plugin.
    Creates a sub-class of the StaticFile which makes writing a file easier.

---
+++ api.rb
@@ -0,0 +1,39 @@
+require "json"
+
+module Jekyll
+  API_DIRECTORY = "api"
+
+  class ApiStaticFile < StaticFile
+    def initialize(site, base, dir, name, content)
+      super(site, base, dir, name)
+
+      @content = content
+    end
+
+    def write(dest)
+      destination = destination(dest)
+
+      directory = File.dirname(destination)
+
+      unless File.exists?(directory)
+        p = Pathname.new(directory)
+        p.mkdir
+      end
+
+      File.open(destination, 'w') do |f|
+        f.write(@content)
+        f.close
+      end
+
+      true
+    end
+  end
+
+  class ApiGenerator < Generator
+    safe true
+
+    def generate(site)
+      site.static_files << ApiStaticFile.new(site, File.expand_path(site.config['destination']), API_DIRECTORY, 'index.html', 'cats')
+    end
+  end
+end
